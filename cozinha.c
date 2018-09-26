c2959a0295be8dcd75458e872f68cf274702674b
Lucas Verdade <lucas.verdade@grad.ufsc.br>
2018-09-25 21:56:52 -0300

    Modificando Cozinha feat Tiz

--- cozinha.c
+++ cozinha.c
@@ -1,19 +1,28 @@
-#ifndef __COZINHA_H__
-#define __COZINHA_H__
-
-#include "pedido.h"
+#include "cozinha.h"
+#include <stdlib.h>
 
 void cozinha_init(int cozinheiros, int bocas, int frigideiras, int garcons, int tam_balcao) {
-	int* cozinheiroBuffer = (int*)malloc(cozinheiros);
-	int* bocasBuffer = (int*)malloc(bocas);
-	int* frigideirasBuffer = (int*)malloc(frigideiras);
-	int* garcosBuffer = (int*)malloc(garcos);
-	int* tam_balcaoBuffer = (int*)malloc(tan_balcao);
+	cozinha_t *cozinha = malloc(sizeof(cozinha_t));
+	*cozinha = (cozinha_t) {
+		.cozinheiroBuffer = (int*)malloc(sizeof(int)*cozinheiros),
+		.bocasBuffer = (int*)malloc(sizeof(int)*bocas),
+		.frigideirasBuffer = (int*)malloc(sizeof(int)*frigideiras),
+		.garcosBuffer = (int*)malloc(sizeof(int)*garcons),
+		.tam_balcaoBuffer = (int*)malloc(sizeof(int)*tam_balcao),
+	};
 }
-void cozinha_destroy() {
-	free(cozinheiroBuffer);
-	free(bocasBuffer);
-	free(frigideirasBuffer);
-	free(garcosBuffer);
-	free(tam_balcaoBuffer);
+void cozinha_destroy(cozinha_t* cozinha) {
+	free(cozinha->cozinheiroBuffer);
+	free(cozinha->bocasBuffer);
+	free(cozinha->frigideirasBuffer);
+	free(cozinha->garcosBuffer);
+	free(cozinha->tam_balcaoBuffer);
+	free(cozinha);
+}
+
+void processar_pedido(pedido_t pedido) {
+	// criar semaforo de acesso ao fogao p os cozinheiros
+	// criar semaforo para acesso ao bacao de pedidos prontos
+
+	
 }
--- cozinha.h
+++ cozinha.h
@@ -3,6 +3,14 @@
 
 #include "pedido.h"
 
+typedef struct {
+	int* cozinheiroBuffer;
+	int* bocasBuffer;
+	int* frigideirasBuffer;
+	int* garcosBuffer;
+	int* tam_balcaoBuffer;
+} cozinha_t;
+
 extern void cozinha_init(int cozinheiros, int bocas, int frigideiras, int garcons, int tam_balcao);
 extern void cozinha_destroy();
 extern void processar_pedido(pedido_t p);
